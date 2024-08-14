e2a1c582be8acbf1eea4568e5c268739b39ca91c
Juanita <114871036+JuanitaCathy@users.noreply.github.com>
2024-08-14 14:42:45 +0000

    star-bg and robo model done

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ import { cn } from "@/lib/utils";
 
 export default function NavbarDemo() {
   return (
-    <div className="relative w-full flex items-center justify-center">
+    <div className="relative w-full flex items-center justify-center">   
       <Navbar className="top-8" />
     </div>
   );
@@ -18,15 +18,15 @@ function Navbar({ className }: { className?: string }) {
       className={cn("fixed top-16 inset-x-0 max-w-4xl mx-auto z-50", className)}
     >
       <Menu setActive={setActive}>
-        <MenuItem setActive={setActive} active={active} item="About" />
+        <MenuItem setActive={setActive} active={active} item="About" href="/" />
         <div className="h-full flex items-center justify-center text-gray-400">•</div>
-        <MenuItem setActive={setActive} active={active} item="Pricing" />
+        <MenuItem setActive={setActive} active={active} item="Pricing" href="#pricing"/>
         <div className="h-full flex items-center justify-center text-gray-400">•</div>
-        <MenuItem setActive={setActive} active={active} item="Contact" />
+        <MenuItem setActive={setActive} active={active} item="Contact" href="#contact"/>
         <div className="h-full flex items-center justify-center text-gray-400">•</div>
-        <MenuItem setActive={setActive} active={active} item="Docs" />
+        <MenuItem setActive={setActive} active={active} item="Services" href="#services"/>
         <div className="h-full flex items-center justify-center text-gray-400">•</div>
-        <MenuItem setActive={setActive} active={active} item="Team" />
+        <MenuItem setActive={setActive} active={active} item="Team" href="#team" />
       </Menu>
     </div>
   );
