6bab690b6676e9ae5ae6f638f00bcd034469dfc1
Juanita <114871036+JuanitaCathy@users.noreply.github.com>
2024-08-14 09:12:42 +0000

    nav-bar created

---
+++ components/Header.tsx
@@ -0,0 +1,33 @@
+"use client";
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem } from "../components/ui/navbar-menu";
+import { cn } from "@/lib/utils";
+
+export default function NavbarDemo() {
+  return (
+    <div className="relative w-full flex items-center justify-center">
+      <Navbar className="top-8" />
+    </div>
+  );
+}
+
+function Navbar({ className }: { className?: string }) {
+  const [active, setActive] = useState<string | null>(null);
+  return (
+    <div
+      className={cn("fixed top-16 inset-x-0 max-w-4xl mx-auto z-50", className)}
+    >
+      <Menu setActive={setActive}>
+        <MenuItem setActive={setActive} active={active} item="About" />
+        <div className="h-full flex items-center justify-center text-gray-400">•</div>
+        <MenuItem setActive={setActive} active={active} item="Pricing" />
+        <div className="h-full flex items-center justify-center text-gray-400">•</div>
+        <MenuItem setActive={setActive} active={active} item="Contact" />
+        <div className="h-full flex items-center justify-center text-gray-400">•</div>
+        <MenuItem setActive={setActive} active={active} item="Docs" />
+        <div className="h-full flex items-center justify-center text-gray-400">•</div>
+        <MenuItem setActive={setActive} active={active} item="Team" />
+      </Menu>
+    </div>
+  );
+}
---
+++ components/ui/navbar-menu.tsx
@@ -0,0 +1,62 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
+
+const transition = {
+  type: "spring",
+  mass: 0.5,
+  damping: 11.5,
+  stiffness: 100,
+  restDelta: 0.001,
+  restSpeed: 0.001,
+};
+
+export const MenuItem = ({
+  setActive,
+  active,
+  item,
+}: {
+  setActive: (item: string) => void;
+  active: string | null;
+  item: string;
+}) => {
+  return (
+    <div onMouseEnter={() => setActive(item)} className="relative">
+      <motion.p
+        transition={{ duration: 0.3 }}
+        className="cursor-pointer text-base font-semibold text-[#d1d5db] hover:opacity-[0.8]"
+      >
+        {item}
+      </motion.p>
+    </div>
+  );
+};
+
+export const Menu = ({
+  setActive,
+  children,
+}: {
+  setActive: (item: string | null) => void;
+  children: React.ReactNode;
+}) => {
+  return (
+    <nav
+      onMouseLeave={() => setActive(null)}
+      className="relative rounded-full border border-[#2c2f41] bg-[#1a1b2e]/50 backdrop-blur-lg shadow-input flex justify-center items-center space-x-8 px-16 py-5" 
+    >
+      {children}
+    </nav>
+  );
+};
+
+export const HoveredLink = ({ children, ...rest }: any) => {
+  return (
+    <Link
+      {...rest}
+      className="text-neutral-700 dark:text-neutral-200 hover:text-black"
+    >
+      {children}
+    </Link>
+  );
+};
