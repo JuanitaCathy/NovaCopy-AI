6b7ba5991bcb0bd0d8e2db107c259ad32aa7b930
Miho Funayama <funamiho1011@gmail.com>
2024-08-21 00:25:37 +0900

    created pages based on navbbar contents

---
+++ app/contact/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+import { ShootingStars } from "@/components/ui/shooting-stars";
+import { StarsBackground } from "@/components/ui/stars-background";
+import NavbarDemo from "@/components/Header";
+
+export default function Contact() {
+  return (
+    <main className="relative min-h-screen overflow-hidden flex flex-col items-center justify-center">
+      {/* Background Effects */}
+      <StarsBackground />
+      <ShootingStars />
+      <div className="z-10 w-full max-w-5xl text-center p-4 md:p-24 mt-24 md:mt-32">
+        <NavbarDemo />
+      <h1 className="text-4xl md:text-5xl font-bold mb-12 mt-12">Contact</h1>
+      </div>
+    </main>
+  );
+}
