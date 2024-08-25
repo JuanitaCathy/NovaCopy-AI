6cbbb110152f83e3f0c2c2a1b9c28cbc920914ae
Juanita <114871036+JuanitaCathy@users.noreply.github.com>
2024-08-25 10:10:43 +0000

    more hero section

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -91,14 +91,9 @@ const FAQSection = () => {
               onChange={handleChange(`panel${item.id}`)}
               className={`bg-transparent border border-white/[0.4] rounded-xl mb-4 max-w-[500px] mx-auto ${
                 expanded === `panel${item.id}` ? "border-[#00b4d8] shadow-lg" : "border-white"
-                // expanded === `panel${item.id}` ? "border-pink-500 shadow-lg" : "dark:border-white/[0.4]"
               }`} 
 
             >
-              {/* <Accordion
-              key={item.id}
-              className="bg-transparent border border-white rounded-2xl mb-4 max-w-[500px] mx-auto"
-            > */}
               <AccordionSummary
                 expandIcon={<ExpandMoreIcon sx={{ color: "white" }} />}
                 aria-controls={`panel${item.id}-content`}
