092c24cd3a0190e4615a37b0635ca243b0afa755
Juanita <114871036+JuanitaCathy@users.noreply.github.com>
2024-08-25 15:08:22 +0000

    Added Analytics tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter,  Calistoga } from "next/font/google";
+import Script from 'next/script';
 import "./globals.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { ConvexClientProvider } from "./ConvexClientProvider ";
@@ -19,9 +20,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-    <body className={`${inter.className}`}>
+      <head>
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+          `}
+        </Script>
+      </head>
+      <body className={`${inter.className}`}>
         <ConvexClientProvider>{children}</ConvexClientProvider>
-        </body>
+      </body>
     </html>
   );
 }
