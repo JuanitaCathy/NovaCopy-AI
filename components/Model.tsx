ae8a33bac18b027607a4dc96d9cc033d18838ca9
Juanita <114871036+JuanitaCathy@users.noreply.github.com>
2024-08-27 08:14:46 +0000

    Robo fixed and feature zIndex fixed

--- components/Model.tsx
+++ components/Model.tsx
@@ -1,35 +1,22 @@
 import { useAnimations, useGLTF } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
 import { useEffect, useRef } from "react";
 import { Group } from "three";
 
 useGLTF.preload("/robot_playground.glb");
 
-export default function Model({
-  animationProgress,
-}: {
-  animationProgress: number;
-}) {
+export default function Model() {
   const group = useRef<Group>(null);
   const { scene, animations } = useGLTF("/robot_playground.glb");
   const { actions } = useAnimations(animations, scene);
 
   useEffect(() => {
     if (actions["Experiment"]) {
-      actions["Experiment"].play().paused = true;
+      actions["Experiment"].play();
     }
   }, [actions]);
 
-  useFrame(() => {
-    if (actions["Experiment"]) {
-      const animationClip = actions["Experiment"].getClip();
-      actions["Experiment"].time =
-        (animationClip.duration * animationProgress) % animationClip.duration;
-    }
-  });
-
   return (
-    <group ref={group} scale={[1.4, 2.2, 0.17]} position={[0.4, -1.5, 0]}>
+    <group ref={group} scale={[1.4, 2.2, 0.17]} position={[-0.1, -1.5, 0]}>
       <primitive object={scene} />
     </group>
   );
