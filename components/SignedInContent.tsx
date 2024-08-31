5ca5ccfca9e1ecff608641f4b1a10fa1e25b4869
Miho Funayama <funamiho1011@gmail.com>
2024-09-01 03:03:38 +0900

    separate page component to display different contents for when signed-in and signed-out

---
+++ components/SignedInContent.tsx
@@ -0,0 +1,10 @@
+import NavbarDemo from "@/components/Header";
+
+export default function SignedInContent() {
+  return (
+    <>
+      <NavbarDemo />
+      <h1>Welcome to your Dashboard</h1>
+    </>
+  );
+}
