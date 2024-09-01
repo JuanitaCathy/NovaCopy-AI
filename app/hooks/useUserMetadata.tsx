ac500c5444596614031f4725774377ff6889bb6c
Miho Funayama <funamiho1011@gmail.com>
2024-09-01 17:04:01 +0900

    added console.log to inspect user data

--- app/hooks/useUserMetadata.tsx
+++ app/hooks/useUserMetadata.tsx
@@ -3,6 +3,9 @@ import { useUser } from '@clerk/nextjs';
 const useUserMetadata = () => {
   const { user } = useUser(); // Ensure user data is being fetched
 
+  // inspect userdata
+  console.log('User data:', user);
+
   // Assuming user contains metadata or you have to handle it differently
   const needsOnboarding = user?.publicMetadata?.onboardingNeeded || false;
 
