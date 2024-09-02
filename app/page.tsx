6971d92ccd29ed6363e0e1cd66348ae0a3154fef
Miho Funayama <funamiho1011@gmail.com>
2024-09-02 21:21:19 +0900

    deleted unnecessary component

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,12 @@
 import { SignedIn, SignedOut } from "@clerk/nextjs";
 import SignedInContent from "@/components/SignedInContent";
 import SignedOutContent from "@/components/SignedOutContent";
-import SignedUpContent from "@/components/SignedUpContent";
 import "./globals.css";
 
 export default function Home() {
   return (
     <>
       <SignedIn>
-        <SignedUpContent /> {/* Handles redirect if user needs onboarding */}
         <SignedInContent /> {/* Display content for signed-in users */}
       </SignedIn>
       <SignedOut>
