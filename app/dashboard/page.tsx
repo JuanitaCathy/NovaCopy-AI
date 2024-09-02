2e45cb55e1c1aa3c2c333e763336291ba1bbb5f3
Miho Funayama <funamiho1011@gmail.com>
2024-09-03 00:00:11 +0900

    added dashboard basic structure

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,55 @@
-import NavbarDemo from "@/components/Header";
-import { ShootingStars } from "@/components/ui/shooting-stars";
-import { StarsBackground } from "@/components/ui/stars-background";
+'use client'
+
+import { useState } from "react";
+import Sidebar from "../../components/Sidebar";
 
 export default function SignedInContent() {
+  const [userInput, setUserInput] = useState("");
+  const [businesses, setBusinesses] = useState(["Business 1"]); // 登録ビジネスのリスト
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setUserInput(event.target.value);
+  };
+
+  const handleAddBusiness = () => {
+    const newBusiness = prompt("新しいビジネスの名前を入力してください:");
+    if (newBusiness) {
+      setBusinesses([...businesses, newBusiness]);
+    }
+  };
+
   return (
-    <main className="relative min-h-screen overflow-hidden flex flex-col items-center justify-center">
-      <StarsBackground />
-      <ShootingStars />
-      <NavbarDemo />
-      <h1>Welcome to your Dashboard</h1>
+    <main className="relative min-h-screen overflow-hidden flex flex-col">
+      <div className="flex flex-grow">
+        {/* Side bar */}
+        <Sidebar businesses={businesses} handleAddBusiness={handleAddBusiness} />
+
+        {/* User Input Field */}
+        <section className="w-1/2 p-4">
+          <h2 className="text-xl font-semibold">Prompt</h2>
+          <textarea
+            className="w-full h-64 p-2 border border-gray-300 rounded"
+            placeholder="Please enter here..."
+            value={userInput}
+            onChange={handleInputChange}
+          ></textarea>
+
+          <h2 className="text-xl font-semibold">Tone</h2>
+          <textarea
+            className="w-full h-30 p-2 border border-gray-300 rounded"
+            placeholder="Please enter here..."
+          ></textarea>
+          <button className="mt-2 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+            Generate
+          </button>
+        </section>
+
+        {/* AI Response Field */}
+        <section className="w-1/2 bg-gray-500 p-4">
+          <h2 className="text-xl font-semibold">AI Response</h2>
+          <div className="h-64 p-2 border border-gray-300 rounded"></div>
+        </section>
+      </div>
     </main>
   );
 }
