5edf40429644534c8beb684b832b84f743501cdd
SyedaFarheen1 <spongheen@gmail.com>
2024-09-08 03:35:22 +0500

    basic ui of settings and feedback page

---
+++ app/settings/page.tsx
@@ -0,0 +1,88 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { StarsBackground } from "@/components/ui/stars-background";
+import { ShootingStars } from "@/components/ui/shooting-stars";
+
+const SidebarItem = ({
+  label,
+  icon,
+  href,
+}: {
+  label: string;
+  icon: JSX.Element;
+  href: string;
+}) => {
+  const router = useRouter();
+
+  return (
+    <div
+      onClick={() => router.push(href)}
+      className="flex items-center px-4 py-2 space-x-2 hover:bg-gradient-to-r from-[#00b4d8] to-[#9b5de5] text-white rounded-md cursor-pointer transition-all duration-300 whitespace-nowrap z-10"
+    >
+      <div className="flex items-center justify-center w-6 h-12">{icon}</div>
+      <span className="text-sm font-medium">{label}</span>
+    </div>
+  );
+};
+
+const Settings: React.FC = () => {
+  const [search, setSearch] = useState("");
+  const router = useRouter();
+
+  return (
+    <div className="relative h-screen overflow-hidden overflow-y-auto">
+      {" "}
+      {/* Background Effects */}
+      <StarsBackground />
+      <ShootingStars />
+      <div className="flex h-full">
+        <aside className="w-60 bg-[#1a1a2e] p-4 shadow-lg z-20">
+          <div className="flex items-center mb-5">
+            <Image
+              src="/NovaCopy_white.png"
+              alt="Avatar"
+              width={40}
+              height={40}
+              className="rounded-full"
+            />
+            <div className="ml-3 mt-5 text-white">
+              <h2 className="text-xl font-semibold">NovaCopy AI</h2>
+              <button className="text-[#00b4d8] hover mt-4">
+                Upgrade your plan
+              </button>
+            </div>
+          </div>
+          <div className="flex flex-col space-y-2">
+            <SidebarItem
+              label="Create Custom Copy"
+              icon={<svg />}
+              href="/copywriter"
+            />
+            <SidebarItem
+              label="Previous Copies"
+              icon={<svg />}
+              href="/previous-copies"
+            />
+            <SidebarItem label="Settings" icon={<svg />} href="/settings" />
+            <SidebarItem label="Feedback" icon={<svg />} href="/feedback" />
+          </div>
+          <div className="m-5 mt-10">
+            <button
+              onClick={() => router.push("/dashboard")}
+              className="bg-gradient-to-r from-[#00b4d8] to-[#9b5de5] text-white px-4 py-2 rounded-md hover:bg-[#0489b1] transition-all duration-300"
+            >
+              Back to Dashboard
+            </button>
+          </div>
+        </aside>
+
+        {/* Main Content */}
+      </div>
+    </div>
+  );
+};
+
+export default Settings;
